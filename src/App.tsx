5e185e1613e161bb270ef27497ff216c72f13cdb
Oleg Grachev <oagrachev@beeline.ru>
2022-08-16 16:12:23 +0300

    lazyload

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import "./App.css";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./features/Header/Header";
 import Footer from "./features/Footer/Footer";
 import LoginScreen from "./screens/LoginScreen/LoginScreen";
 import ContentScreen from "./screens/ContentScreen/ContentScreen";
+import "./App.css";
 
 function App() {
   return (
--- src/screens/ContentScreen/ContentScreen.tsx
+++ src/screens/ContentScreen/ContentScreen.tsx
@@ -10,14 +10,16 @@ function ContentScreen() {
 
   useEffect(() => {
     if (isAuth) getContent().then((res) => setKittyImg(res.data.src));
-  }, []);
+  }, [isAuth]);
 
   return (
     <>
       {!isAuth && <Navigate to="/login" replace={true} />}
       <div className="pageLayout wrapper flex_col">
         <h1 className="title">Ok, here's your cat!</h1>
-        {kittyImg && <img src={kittyImg} alt="kitty" className="kitty" />}
+        {kittyImg && (
+          <img src={kittyImg} alt="kitty" className="kitty" loading="lazy" />
+        )}
       </div>
     </>
   );
