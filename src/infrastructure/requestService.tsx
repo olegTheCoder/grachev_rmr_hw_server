aed0a0cee9c7ac0e59ff83003fe7f0a377d803eb
Oleg Grachev <oagrachev@beeline.ru>
2022-08-18 13:42:17 +0300

    try/catch fixes

--- src/infrastructure/requestService.tsx
+++ src/infrastructure/requestService.tsx
@@ -5,55 +5,71 @@ type LoginDataType = {
 };
 
 export async function sendLoginData(data: LoginDataType): Promise<any> {
-  const request = await fetch("/api/v1/login", {
-    method: "POST",
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
+  try {
+    const request = await fetch("/api/v1/login", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
 
-  const response = await request.json();
-  return response;
+    const response = await request.json();
+    return response;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export async function getContent(): Promise<any> {
-  const request = await fetch("/api/v1/kitty", {
-    method: "GET",
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  try {
+    const request = await fetch("/api/v1/kitty", {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-  const response = await request.json();
-  return response;
+    const response = await request.json();
+    return response;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export async function getUser(): Promise<any> {
-  const request = await fetch("/api/v1/profile", {
-    method: "GET",
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  try {
+    const request = await fetch("/api/v1/profile", {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-  const response = await request.json();
-  return response;
+    const response = await request.json();
+    return response;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export async function getLogoutServer(): Promise<any> {
-  const request = await fetch("/api/v1/logout", {
-    method: "POST",
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({})
-  });
+  try {
+    const request = await fetch("/api/v1/logout", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({}),
+    });
 
-  const response = await request.json();
-  return response;
+    const response = await request.json();
+    return response;
+  } catch (err) {
+    console.log(err);
+  }
 }
